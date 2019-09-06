71311284b61400fbddffe0a60882d55fa22f4b09
Sivaprrasad Uppalapati <usivaprasad95@gmail.com>
2019-09-06 10:06:40 -0400

    Initial Commit - With 2 JUnit test cases.

---
+++ tests/ShoppingCartTest.java
@@ -0,0 +1,58 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShoppingCartTest {
+
+	@Test
+	public void testCreateCart() {
+		//**************************
+		//assertEquals(expected,actual)
+		//if expected == actual: return true
+		//else: return false
+		//assertEquals(1,1);
+		//assertEquals(1,1);
+		//fail("Not yet implemented");
+		//****************************
+		
+		//1. Make a new cart.
+		ShoppingCart cart = new ShoppingCart();
+		
+		//2. Check number of items in the cart
+		int a = cart.getItemCount();
+		
+		//3. Do the asserts
+		assertEquals(0,a);
+		
+	}
+	
+	@Test
+	public void testEmptyTheCart() {
+		
+		//1. Make a new cart.
+		ShoppingCart cart = new ShoppingCart();
+		
+		//2. Add item to the cart
+		Product phone = new Product("iPhone", 1500);
+		Product hamburger = new Product("burger", 10);
+		
+		cart.addItem(phone);
+		cart.addItem(hamburger);
+				
+		//3. Remove item from the cart.
+		cart.empty();
+		
+				
+		//4. Check number of items in the cart 
+		assertEquals(0, cart.getItemCount());
+		
+	}
+	
+	//1.make 1 function per test case
+	//2.put the @Test annotation on top of the class
+	//3.Write code for your test case
+	//4.Call assertEquals() in your test case
+	//5.Run the program
+	//6.look at the results
+
+}
